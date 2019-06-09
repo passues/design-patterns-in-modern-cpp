bb0c7f2812bdfeb7fdb886c9107007ab3a6f9b14
Charles <qinghuang.ustc@gmail.com>
2019-06-09 15:13:25 -0400

    singleton exercise

---
+++ exercise/singleton/singleton.cpp
@@ -0,0 +1,13 @@
+#include <functional>
+using namespace std;
+
+struct SingletonTester
+{
+  template <typename T>
+  bool is_singleton(function<T*()> factory)
+  {
+    auto a = factory();
+    auto b = factory();
+    return a==b;
+  }
+};
