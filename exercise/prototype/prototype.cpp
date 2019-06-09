65cc3673ac8f1bc87708c75d7bf158991c67fbf9
Charles <qinghuang.ustc@gmail.com>
2019-06-09 14:21:22 -0400

    prototype exercise

---
+++ exercise/prototype/prototype.cpp
@@ -0,0 +1,31 @@
+struct Point
+{
+  int x{ 0 }, y{ 0 };
+
+  Point(){}
+  
+  Point(const int x, const int y) : x{x}, y{y} {}
+};
+
+struct Line
+{
+  Point *start, *end;
+  
+  Line(Point* const start, Point* const end)
+    : start(start), end(end)
+  {
+  }
+
+  ~Line()
+  {
+    delete start;
+    delete end;
+  }
+
+  Line deep_copy() const
+  {
+    Point* new_start = new Point(start->x, start->y);
+    Point* new_end  = new Point(end->x, end->y);
+    return Line(new_start, new_end);
+  }
+};
